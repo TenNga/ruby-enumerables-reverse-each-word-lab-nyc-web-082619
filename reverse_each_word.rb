a2a8c7dca27b22612c20418ef9de4a6f7fde622b
Kevin Karma <kevinkarma55@gmail.com>
2019-08-14 13:40:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,6 @@ end
 
 
 def reverse_each_word(sentence) 
-  reversed = []
   words = sentence.split(" ")
-  words.each { |word| reversed.push(word.reverse) }
-  reversed.join(" ")
+  words.each { |word| word.reverse }
 end
